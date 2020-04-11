55f84f0915dc8e6ead25ef5c89f4413262efc6b8
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2020-04-11 16:50:28 +0200

    Add Styled Components and create GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './Main'
+import GlobalStyles from './styles/global'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Main />
   </React.StrictMode>,
   document.getElementById('root')
