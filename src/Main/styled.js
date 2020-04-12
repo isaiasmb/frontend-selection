57cbac09dbf21e9ec5fe5f79fea3bb2850b9c34b
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2020-04-12 18:44:09 +0200

    Add main layout

---
+++ src/Main/styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const MainWrapper = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`
+
+export const Main = styled.div`
+  padding: 50px 0;
+  text-align: center;
+`
+
+export const MainTitle = styled.h1`
+  font-size: 1.6rem;
+  margin: 0.5rem auto 1.5rem;
+  text-align: center;
+`
+
+export const MainSubtitle = styled.span`
+  display: block;
+  font-size: 1.2rem;
+  font-weight: 300;
+`
+
+export const MainDescription = styled.p`
+  font-size: 1rem;
+  font-weight: 300;
+  line-height: 1.4;
+  text-align: center;
+`
